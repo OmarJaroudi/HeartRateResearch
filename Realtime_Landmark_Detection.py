5577ff36882d46a6d691a6719842a44048534f02
OmarJaroudi <30807936+OmarJaroudi@users.noreply.github.com>
2019-08-18 18:14:34 +0300

    Add files via upload
    Can detect forehead ROI (rectangle) with predefined arbitrary width and height

--- Realtime_Landmark_Detection.py
+++ Realtime_Landmark_Detection.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import cv2
 import dlib
 import numpy as np
@@ -6,95 +8,55 @@ import imutils
 from collections import OrderedDict
 
 LANDMARK_PTS = OrderedDict([
-    ("mouth", (48, 68)),
-  ("right_eyebrow", (17, 22)),
-  ("left_eyebrow", (22, 27)),
-  ("right_eye", (36, 42)),
-  ("left_eye", (42, 48)),
-  ("nose", (27, 35)),
-  ("jaw", (0, 17))
+    ('mouth', (48, 68)),
+    ('right_eyebrow', (17, 22)),
+    ('left_eyebrow', (22, 27)),
+    ('right_eye', (36, 42)),
+    ('left_eye', (42, 48)),
+    ('nose', (27, 35)),
+    ('jaw', (0, 17)),
     ])
 
-def visualize_facial_landmarks(image, shape, colors=None, alpha=0.75):
-  # create two copies of the input image -- one for the
-  # overlay and one for the final output image
-  overlay = image.copy()
-  output = image.copy()
- 
-  # if the colors list is None, initialize it with a unique
-  # color for each facial landmark region
-  if colors is None:
-    colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
-      (168, 100, 168), (158, 163, 32),
-      (163, 38, 32), (180, 42, 220)]
-        
-# loop over the facial landmark regions individually
-  for (i, name) in enumerate(LANDMARK_PTS.keys()):
-    # grab the (x, y)-coordinates associated with the
-    # face landmark
-    (j, k) = LANDMARK_PTS[name]
-    pts = shape[j:k]
- 
-    # check if are supposed to draw the jawline
-    if name == "jaw":
-      # since the jawline is a non-enclosed facial region,
-      # just draw lines between the (x, y)-coordinates
-      for l in range(1, len(pts)):
-        ptA = tuple(pts[l - 1])
-        ptB = tuple(pts[l])
-        cv2.line(overlay, ptA, ptB, colors[i], 2)
- 
-    # otherwise, compute the convex hull of the facial
-    # landmark coordinates points and display it
-    else:
-      hull = cv2.convexHull(pts)
-      cv2.drawContours(overlay, [hull], -1, colors[i], -1)
-            
-    # apply the transparent overlay
-  cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
- 
-  # return the output image
-  return output
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("Face_Predictor.dat")
+predictor = dlib.shape_predictor('Face_Predictor.dat')
 
 cap = cv2.VideoCapture(0)
 
-while(True):
-    ret,frame = cap.read()
+while True:
+   
+    (ret, frame) = cap.read()
     image = frame
+    
     image = imutils.resize(image, width=500)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # detect faces in the grayscale image
+
     rects = detector(gray, 1)
+
     # loop over the face detections
+
     for (i, rect) in enumerate(rects):
-      # determine the facial landmarks for the face region, then
-      # convert the landmark (x, y)-coordinates to a NumPy array
-      shape = predictor(gray, rect)
-      shape = face_utils.shape_to_np(shape)
-     
-      # loop over the face parts individually
-      for (name, (i, j)) in LANDMARK_PTS.items():
-            # clone the original image so we can draw on it, then
-            # display the name of the face part on the image
-            clone = image.copy()
-            (x, y, w, h) = cv2.boundingRect(np.array([shape[28:35]]))
-            height = h/2
-            print(height)
-            roi = image[y:y + h, x:x + w]
-            roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
-            # show the particular face part
-            cv2.imshow("ROI", roi)
-            cv2.imshow("Image", clone)
-      
-     
-      # visualize all facial landmarks with a transparent overlay
-#     output = face_utils.visualize_facial_landmarks(image, shape)
-#     cv2.imshow("Image", output)
-      if cv2.waitKey(1) == 13: #13 is the Enter Key
-            break
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        (x, y,s,d) = cv2.boundingRect(np.array([shape[27]]))
+        (a,b) = shape[35]
+#        y = 2*y-b
+        w = 20
+        h = 8
+        roi = image[y-h:y+h,x-w:x+w]
+        
+        roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
+        cv2.imshow('ROI',roi)
+        cv2.rectangle(image,(x-w,y-h),(x+w,y+h),(0,0,255),1)
+        
+              
+            
+        cv2.imshow('Image', image)
+                
+
+    if cv2.waitKey(1) == 13:  # 13 is the Enter Key
+        break
 cap.release()
 cv2.destroyAllWindows()
